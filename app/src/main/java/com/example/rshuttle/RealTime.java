d2bd3cadaefdaed31ef3859553dee5980e023495
devjw2 <mcjeffrey168@gmail.com>
2019-02-16 16:59:22 -0500

    justin's files v2

---
+++ app/src/main/java/com/example/rshuttle/RealTime.java
@@ -0,0 +1,191 @@
+package com.example.rshuttle;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.example.rshuttle.RealTimeInformation;
+
+/**
+ * This class will contain all implementation of the methods real time data required for the
+ * RShuttle App.
+ *
+ * NOTE:
+ *      RIT's geo-area: 43.077091,-77.690163|43.093377,-77.652302
+ *      RIT's agencyId: 643
+ *
+ * @author Justin Yau
+ */
+public class RealTime implements RealTimeInformation {
+
+    // This is the api key we use to access openAPI
+    public static final String openAPIKey = "7a9a8dd817mshe07f2c506c8d832p1ff971jsnb0ff5652a428";
+
+    public RealTime(String agencyId) {
+        float[][] locations = busLocations(agencyId, "4012168");
+        for(int i = 0; i < locations.length; i++) {
+            System.out.println("Latitude: " + locations[i][0] + " Longitude: " + locations[i][1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        RealTime r = new RealTime("643");
+    }
+
+    /**
+     * This method makes a post request to the given url (has to be for rapidapi) and returns the
+     * data array.
+     *
+     * @param url - The url of the request
+     * @param agencyId - The agency id of the college that you want to find information about
+     * @return - The data array
+     * @return - NULL if error occurred
+     * @author Justin Yau
+     */
+    public JSONArray makePostRequestOpenAPI(String url, String agencyId) {
+        try {
+            HttpResponse<JsonNode> response = Unirest.get(url)
+                    .header("X-RapidAPI-Key", openAPIKey)
+                    .asJson();
+            JSONObject responseJson = response.getBody().getObject();
+            JSONObject data = responseJson.getJSONObject("data");
+            JSONArray set = data.getJSONArray(agencyId);
+            return set;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * This method will return the bus locations associated with the agencyID (College) and routeID.
+     *
+     * @param agencyId - The id of the college that you want to find the bus locations for
+     * @return - The bus locations associated with the agencyID and routeID.
+     * @return - NULL if an error occurred
+     * @author Justin Yau
+     */
+    public float[][] busLocations(String agencyId, String routeID) {
+        try {
+            JSONArray loc = makePostRequestOpenAPI("https://transloc-api-1-2.p.rapidapi.com/vehicles.json?routes=" + routeID + "&callback=call&agencies=" + agencyId, agencyId);
+            if(loc == null) { return null; }
+            float[][] locations = new float[loc.length()][2];
+            for(int i = 0; i < loc.length(); i++) {
+                JSONObject location = loc.getJSONObject(i);
+                JSONObject coordinates = location.getJSONObject("location");
+                locations[i][0] = Float.parseFloat(coordinates.getString("lat"));
+                locations[i][1] = Float.parseFloat(coordinates.getString("lng"));
+            }
+            return locations;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /***
+     * This method will return all the routes served by the inputted college
+     * READ:
+     *      Map has routeIDs as keys and a list of stop ids as values
+     *      The first entity in the list will be the name of the route
+     *      To get cords of stop ids, call the stop function and look it up
+     *
+     * @param agencyId - The id of the college that you want to find the routes for
+     * @return - All the routes that are served by the college
+     * @return - NULL if error occurred
+     * @author Justin Yau
+     */
+    @Override
+    public Map<String, List<String>> routes(String agencyId) {
+        try {
+            JSONArray r = makePostRequestOpenAPI("https://transloc-api-1-2.p.rapidapi.com/routes.json?callback=call&agencies=" + agencyId, agencyId);
+            if(r == null) { return null; }
+            Map<String, List<String>> routes = new HashMap<String, List<String>>();
+            for(int i = 0; i < r.length(); i++) {
+                JSONObject route = r.getJSONObject(i);
+                List<String> s = new ArrayList<>();
+                s.add(route.getString("long_name"));
+                JSONArray stops = route.getJSONArray("stops");
+                for(int j = 0; j < stops.length(); j++) {
+                    String stopID = stops.getString(j);
+                    s.add(stopID);
+                }
+                routes.put(route.getString("route_id"), s);
+            }
+            return routes;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /***
+     * This method will return all stops serves the agencyID and their respective coordinates
+     *
+     * READ:
+     *      The map will be a collection of stopIds as the key and their coordinate as the value.
+     *      USE in-cojunction with the routes method to make connections
+     *
+     * @param agencyId - The id of the college that you want to find the routes for
+     * @return - All the routes that are served by the college and their coordinates
+     * @return - NULL is an error occurred
+     * @author Justin Yau
+     */
+    @Override
+    public Map<String, String[]> stops(String agencyId) {
+        try {
+            JSONArray r = makePostRequestOpenAPI("https://transloc-api-1-2.p.rapidapi.com/stops.json?callback=call&agencies=" + agencyId, agencyId);
+            if(r == null) { return null; }
+            Map<String, String[]> stops = new HashMap<String, String[]>();
+            for(int i = 0; i < r.length(); i++) {
+                JSONObject stop = r.getJSONObject(i);
+                String[] info = new String[3];
+                info[0] = stop.getString("name");
+                JSONObject cord = stop.getJSONObject("location");
+                info[1] = cord.getString("lat");
+                info[2] = cord.getString("lng");
+            }
+            return stops;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /***
+     * This method will return all arrival times at the stopID that is served by agencyID
+     *
+     * READ:
+     *      Hashmap will have routeIDs as the key values and their estimated arrival time as values
+     *
+     * @return - All the route ids and their respective arrival times
+     * @return - Null if an error occurred
+     * @author Justin Yau
+     */
+    public Map<String, String> timeAtStop(String agencyId, String stopId) {
+        try {
+            JSONArray data = makePostRequestOpenAPI("https://transloc-api-1-2.p.rapidapi.com/arrival-estimates.json?stops=" + stopId + "&callback=call&agencies=" + agencyId, agencyId);
+            if(data == null) { return null; }
+            Map<String, String> times = new HashMap<String, String>();
+            for(int i = 0; i < data.length(); i++) {
+                JSONArray arrivals = data.getJSONObject(i).getJSONArray("arrivals");
+                for(int j = 0; j < arrivals.length(); j++) {
+                    JSONObject info = arrivals.getJSONObject(j);
+                    times.put(info.getString("route_id"), info.getString("arrival_at"));
+                }
+            }
+            return times;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
---
+++ app/src/main/java/com/example/rshuttle/RealTimeInformation.java
@@ -0,0 +1,82 @@
+package com.example.rshuttle;
+
+import org.json.JSONArray;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This interface will contain all the methods real time data required for the RShuttle App
+ *
+ * NOTE:
+ *      RIT's geo-area: 43.077091,-77.690163|43.093377,-77.652302
+ *      RIT's agencyId: 643
+ *
+ * @author Justin Yau
+ */
+public interface RealTimeInformation {
+
+    /**
+     * This method makes a post request to the given url (has to be for rapidapi) and returns the
+     * data array.
+     *
+     * @param url - The url of the request
+     * @param agencyId - The agency id of the college that you want to find information about
+     * @return - The data array
+     * @return - NULL if error occurred
+     * @author Justin Yau
+     */
+    public JSONArray makePostRequestOpenAPI(String url, String agencyId);
+
+    /**
+     * This method will return the bus locations associated with the agencyID (College) and routeID.
+     *
+     * @param agencyId - The id of the college that you want to find the bus locations for
+     * @return - The bus locations associated with the agencyID and routeID.
+     * @return - NULL if an error occurred
+     * @author Justin Yau
+     */
+    public float[][] busLocations(String agencyId, String routeID);
+
+    /***
+     * This method will return all the routes served by the inputted college
+     * READ:
+     *      Map has routeIDs as keys and a list of stop ids as values
+     *      The first entity in the list will be the name of the route
+     *      To get cords of stop ids, call the stop function and look it up
+     *
+     * @param agencyId - The id of the college that you want to find the routes for
+     * @return - All the routes that are served by the college
+     * @return - NULL if error occurred
+     * @author Justin Yau
+     */
+    public Map<String, List<String>> routes(String agencyId);
+
+    /***
+     * This method will return all stops serves the agencyID and their respective coordinates
+     *
+     * READ:
+     *      The map will be a collection of stopIds as the key and their coordinate as the value.
+     *      USE in-cojunction with the routes method to make connections
+     *
+     * @param agencyId - The id of the college that you want to find the routes for
+     * @return - All the routes that are served by the college and their coordinates
+     * @return - NULL is an error occurred
+     * @author Justin Yau
+     */
+    public Map<String, String[]> stops(String agencyId);
+
+    /***
+     * This method will return all arrival times at the stopID that is served by agencyID
+     *
+     * READ:
+     *      Hashmap will have routeIDs as the key values and their estimated arrival time as values
+     *
+     * @return - All the route ids and their respective arrival times
+     * @return - Null if an error occurred
+     * @author Justin Yau
+     */
+    public Map<String, String> timeAtStop(String agencyId, String stopId);
+
+}
